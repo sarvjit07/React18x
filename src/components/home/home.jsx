4c3390c3c814f1e22b0c09f3d1e391c368cdc511
sarvjit07 <sarvjitlala@gmail.com>
2024-12-05 12:49:31 +0530

    adding Bootstrap and use that in component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,13 @@
 import "./home.css"
 export function Home() {
   return (
-    <div>
-      <h2 className="box">React Home</h2>
+    <div className="container-fluid">
+      <h2 className="box bg-warning m-4"> <span className="bi bi-house-door-fill"></span> React Home</h2>
+      <div className="alert alert-dismissible alert-success w-25 m-4">
+      <h4>Welcome to React 18</h4>
+      <p>JavaScript library for bulding Native and Web UI.</p>
+      <button className="btn btn-success" data-bs-dismiss="alert">OK</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Home } from './components/home/home';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import bootstrap from "bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
