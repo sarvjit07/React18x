a978609bd76e4e03d8ca5c6dbad7017636974329
sarvjit07 <sarvjitlala@gmail.com>
2024-12-07 10:33:17 +0530

    use fakestoreAPI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Home } from './components/home/home';
+// import { Home } from './components/home/home';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import bootstrap from "bootstrap";
+import { FakestoreProducts } from './components/TimeEvents/fakestore-products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <FakestoreProducts/>
   </React.StrictMode>
 );
 
